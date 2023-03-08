0f43b9c19155073531b7e7f8ebb82ca086bdce05
anthony <devgayflor@gmail.com>
2023-03-07 22:32:43 -0500

    added type casting for the server component return as a Promise<JSX.Element>

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <React.Fragment >
       <Header />
+      {/* @ts-expect-error Server Component */}
       <ActiveListings />
       <ContactMe />
       <Socials />
